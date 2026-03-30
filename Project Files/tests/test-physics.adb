281cb1ed14d87294fbb9ffdbc6e53ed86924a556
Oliver Hill <Oliverhill@u.boisestate.edu>
2026-03-30 13:12:50 -0600

    Test-Physics starter code

---
+++ Project Files/tests/test-physics.adb
@@ -0,0 +1,12 @@
+with Ada.Text_IO; use Ada.Text_IO;
+with Physics;
+
+procedure Test_Physics is
+
+   Result : Long_Float;
+   
+begin
+   -- TODO: call Tsiolkovsky with known values, check result is correct
+   Put_Line ("Physics tests passed.");
+   
+end Test_Physics;
