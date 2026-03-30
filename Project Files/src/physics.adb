4e63bd024213a89651bd27695e3b7155a53963fd
Oliver Hill <Oliverhill@u.boisestate.edu>
2026-03-30 13:12:27 -0600

    Added Physics starter code

---
+++ Project Files/src/physics.adb
@@ -0,0 +1,18 @@
+with Ada.numerics.Elementary_Functions;
+
+package body physics is
+
+   function Tsiolkovsky (Ve : Long_Float; M0 : Long_Float; Mf : Long_Float)
+
+      return Long_Float is
+
+   begin 
+
+   -- TODO: implement delta-V calculation
+   -- hint: Ve * Log(M0 / Mf)
+
+      return 0.0;
+   
+   end Tsiolkovsky;
+
+end physics;
---
+++ Project Files/src/physics.ads
@@ -0,0 +1,8 @@
+package physics is
+
+   function Tsiolkovsky  (Ve: Long_Float; 
+                          M0: Long_Float; 
+                          Mf: Long_Float)
+   return Long_Float;
+
+end physics;   
